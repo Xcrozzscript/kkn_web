c6608cf7259d4e4079c2081d8db9cb2829a6c29e
= <=>
2023-07-21 23:04:50 +0700

    database sementara septa

---
+++ database/ds18b20.php
@@ -0,0 +1,67 @@
+<?php
+  require 'database.php';
+
+  if (!empty($_POST)) {
+    $id = $_POST['id'];
+    $suhu = $_POST['suhu'];
+    $ph = $_POST['ph'];
+    $salinitas = $_POST['salinitas'];
+//...
+    date_default_timezone_set("Asia/Jakarta");
+    $tm = date("H:i:s");
+    $dt = date("Y-m-d");
+    //........................................
+    //...
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //
+    $sql = "UPDATE esp32_table SET suhu = ?, ph = ?, salinitas = ?, time = ?, date = ? WHERE id = ?";
+    $q = $pdo->prepare($sql);
+    $q->execute(array($suhu,$ph,$salinitas,$tm,$dt,$id));
+    Database::disconnect();
+    //........................
+    //...
+    $id_key;
+    $board = $_POST['id'];
+    $found_empty = false;
+
+    $pdo = Database::connect();
+
+    while ($found_empty == false) {
+      $id_key = generate_string_id(10);
+
+      $sql = 'SELECT * FROM esp32_table WHERE id="' . $id_key . '"';
+      $q = $pdo->prepare($sql);
+      $q->execute();
+
+      if (!$data = $q->fetch()) {
+        $found_empty = true;
+      }
+    }
+    //::::::::
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$sql = "INSERT INTO esp32_table (id,board,suhu,ph,salinitas,time,date) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id_key,$board,$suhu,$ph,$salinitas,$tm,$dt));
+    //::::::::
+    
+    Database::disconnect();
+    //........................................ 
+  }
+  //---------------------------------------- 
+  
+  //---------------------------------------- Function to create "id" based on numbers and characters.
+  function generate_string_id($strength = 16) {
+    $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $input_length = strlen($permitted_chars);
+    $random_string = '';
+    for($i = 0; $i < $strength; $i++) {
+      $random_character = $permitted_chars[mt_rand(0, $input_length - 1)];
+      $random_string .= $random_character;
+    }
+    return $random_string;
+  }
+  //---------------------------------------- 
+?>
---
+++ database/getdata.php
@@ -0,0 +1,31 @@
+<?php
+  include 'database.php';
+
+  if (!empty($_POST)) {
+
+    $id = $_POST['id'];
+
+    $myObj = (object)array();
+    
+    //........................................ 
+    $pdo = Database::connect();
+
+    $sql = 'SELECT * FROM esp32_table WHERE id="' . $id . '"';
+    foreach ($pdo->query($sql) as $row) {
+      $date = date_create($row['date']);
+      $dateFormat = date_format($date,"d-m-Y");
+      
+      $myObj->id = $row['id'];
+      $myObj->suhu = $row['suhu'];
+      $myObj->ph = $row['ph'];
+      $myObj->salinitas = $row['salinitas'];
+      $myObj->ls_time = $row['time'];
+      $myObj->ls_date = $dateFormat;
+
+      $myJSON = json_encode($myObj);
+
+      echo $myJSON;
+    }
+    Database::disconnect();
+  }
+?>
